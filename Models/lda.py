8c1d97af9a6044851318572581844bc38573c08a
Cryan <cryanzpj@gmail.com>
2016-11-10 18:24:22 -0500

    update data

--- Models/lda.py
+++ Models/lda.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+sys.path.insert(0, '/Users/cryan/Desktop/Project/NVDM-For-Document-Classification/Models')
 from make_corp import *
 import gensim
 from sklearn.multiclass import OneVsRestClassifier
@@ -6,51 +8,55 @@ from sklearn.svm import LinearSVC
 from sklearn import metrics
 
 from sklearn.datasets import fetch_20newsgroups, fetch_rcv1
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
-from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.linear_model import RidgeClassifier
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.naive_bayes import BernoulliNB, MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import NearestCentroid
-from sklearn.utils.extmath import density
 from sklearn import metrics
-from sklearn.datasets import  make_multilabel_classification
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
 
 
-
-def train_lsi(corp,id2word,n_topic =100):
+def train_lsi(corp,id2word,n_topic =100,test = None):
     lsi = gensim.models.lsimodel.LsiModel(corpus=np.array(corp),id2word=id2word,num_topics = n_topic)
     #lda = gensim.models.hdpmodel.HdpModel(corp, id2word=id2word)
 
-    res = np.zeros((len(corp),n_topic))
+    train_feature = np.zeros((len(corp),n_topic))
     for i,j in enumerate(corp):
         temp = np.asarray(lsi[j])
-        res[i,temp[:,0].astype(int)] = temp[:,1]
-    return res
+        train_feature[i,temp[:,0].astype(int)] = temp[:,1]
+
+    if test:
+        test_feature = np.zeros((len(test),n_topic))
+        for i,j in enumerate(test):
+            temp = np.asarray(lsi[j])
+            test_feature[i,temp[:,0].astype(int)] = temp[:,1]
+
+    return lsi,train_feature,test_feature
 
 if __name__ == "__main__":
-    file_dir = "../data/RCV1/token/lyrl2004_tokens_train.dat"
+    train_dir = "../data/RCV1/token/lyrl2004_tokens_train.dat"
     vocab_dir =  "../data/RCV1/token/lyrl2004_tokens_train.txt"
+    test_dir = "../data/RCV1/token/lyrl2004_tokens_test_pt0.dat"
+    labels = "../data/RCV1/token/rcv1-v2.topics.qrels"
+
 
     #load data and convert to corpus
     vocab = vocab_to_dict(vocab_dir)
-    corp = make_corp(file_dir,vocab)
+    corp = make_corp(train_dir,vocab)
     id2word = dict(np.array(vocab.items(),dtype=object)[:,::-1])
 
-    #train lda model and get document representations
-    lsi = train_lda(corp,id2word,200)
-    # lda.get_document_topics(corp,minimum_probability= 0.0)
+    test = make_corp(test_dir,vocab)
+
+    #train lsi model and get document representations
+    lsi,data_train,data_test = train_lsi(corp,id2word,2000,test)
+
+    label = fetch_rcv1(data_home="../data/RCV1/vec",download_if_missing=False)
+
+    model = OneVsRestClassifier(PassiveAggressiveClassifier(n_iter = 50),n_jobs=-1)
+    model.fit(data_train, label.target[:data_train.shape[0]])
+    res1  =model.predict(data_test)
+
+    metrics.f1_score(label.target[data_train.shape[0]:data_train.shape[0]+data_test.shape[0]], res1 , average="micro")
 
 
-    res1 =  OneVsRestClassifier(LinearSVC()).fit(lsi, data_train.target).predict(lsi)
-    metrics.f1_score(data_train.target, res1 , average="micro")
-
-    vector_model = LsiModel(corpus=np.array(corp), num_topics=100, id2word=id2word)
-
-    data_train.target[2001]
--- Models/make_corp.py
+++ Models/make_corp.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy as sp
+import re
 from collections import Counter,defaultdict
 
 
@@ -6,34 +8,114 @@ def vocab_to_dict(file):
     vocab_list = open(file).readlines()
     return dict(np.array(map(lambda x: tuple(x.strip().split(' ')[:2]),vocab_list),dtype='|S51,i4'))
 
-def make_corp(file,vocab):
+def clean_str(string):
+
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+
+
+
+def make_corp(file,model,vocab = None):
     file = open(file)
     corpus = []
     c_id,c = 0,Counter()
 
-    while True:
-        next_ = file.readline()
-        if next_ == '':
-            break
-        if next_ == '\n':
-            continue
-        else:
-            next_ = next_.strip()
-            if next_.split(' ')[0] != '.I'  and next_.split(' ')[0] != '.W' :
-                c += Counter(next_.split(' '))
-            elif next_.split(' ')[0] == '.I':
-                if c_id == 0:
-                    c_id,c = int(next_.split(' ')[1]),Counter()
-                else:
-                    for key in c.keys():
-                        c[vocab[key]] = c.pop(key)
-                    corpus.append(map(lambda x: (x[0],x[1]),c.items()))
-                    c_id,c = int(next_.split(' ')[1]),Counter()
-    for key in c.keys():
-        c[vocab[key]] = c.pop(key)
-    corpus.append(map(lambda x: (x[0],x[1]),c.items()))
+    # making corpus for LDA model
+    if vocab and model == 'LDA':
+        while True:
+            next_ = file.readline()
+            if next_ == '':
+                break
+            if next_ == '\n':
+                continue
+            else:
+                next_ = next_.strip()
+                if next_.split(' ')[0] != '.I'  and next_.split(' ')[0] != '.W' :
+                    c += Counter(next_.split(' '))
+                elif next_.split(' ')[0] == '.I':
+                    if c_id == 0:
+                        c_id,c = int(next_.split(' ')[1]),Counter()
+                    else:
+                        for key in c.keys():
+                            try:
+                                vocab[key]
+                            except KeyError:
+                                c.pop(key)
+                            else:
+                                c[vocab[key]] = c.pop(key)
+                        corpus.append(map(lambda x: (x[0],x[1]),c.items()))
+                        c_id,c = int(next_.split(' ')[1]),Counter()
+        for key in c.keys():
+            try:
+                vocab[key]
+            except KeyError:
+                c.pop(key)
+            else:
+                c[vocab[key]] = c.pop(key)
+        corpus.append(map(lambda x: (x[0],x[1]),c.items()))
 
-    return corpus
+        return corpus
+    #making data for NVDM model
+    elif model == "NVDM":
+        corpus_Count = []
+        while True:
+            next_ = file.readline()
+            if next_ == '':
+                break
+            if next_ == '\n':
+                continue
+            else:
+                next_ = next_.strip()
+                if next_.split(' ')[0] != '.I'  and next_.split(' ')[0] != '.W' :
+                    #vocab+= Counter(next_.split(' '))
+                    corpus += next_.split(" ")
+                    c+=Counter(next_.split(" "))
+                elif next_.split(' ')[0] == '.I':
+                    if c_id == 0:
+                        c_id,c = int(next_.split(' ')[1]),Counter()
+                    else:
+                        corpus_Count.append(c)
+                        c_id,c = int(next_.split(' ')[1]),Counter()
+        corpus_Count.append(c)
+
+        if not vocab:
+            vocab = Counter(corpus)
+            for i in vocab.keys():
+                if clean_str(i) == "":
+                    vocab.pop(i)
+            vocab = dict(zip(np.asarray([("<UNK>",1)] + vocab.most_common(9999))[:,0],range(10000)))
+        else:
+            pass
+
+        out = np.zeros((len(corpus_Count),10000))
+        for i,j in enumerate(corpus_Count):
+            for ele in j.keys():
+                try: vocab[ele]
+                except KeyError:
+                    j['<UNK>'] += j.pop(ele)
+
+            temp = np.array([[vocab[ele[0]], int(ele[1])]  for ele in j.items() ])
+            temp = temp[np.argsort(temp[:,0])]
+            out[i,temp[:,0]] = temp[:,1]
+
+        if not vocab:
+            return sp.sparse.csr_matrix(out),vocab
+        else:
+            return sp.sparse.csr_matrix(out)
+
 
 
 def make_label(file_):
@@ -49,5 +131,4 @@ def make_label(file_):
         labels[i,j[-1]] = 1
 
     return labels
-
 
